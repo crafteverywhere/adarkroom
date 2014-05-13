fc71fe77175a6ad57884318f4e57d042d7b2dd35
crafteverywhere <1196377271@qq.com>
2014-05-13 21:38:58 +0800

    Update global.js

--- script/events/global.js
+++ script/events/global.js
@@ -3,33 +3,33 @@
  **/
 Events.Global = [
 	{ /* The Thief */
-		title: 'The Thief',
+		title: '盗贼',
 		isAvailable: function() {
 			return (Engine.activeModule == Room || Engine.activeModule == Outside) && $SM.get('game.thieves') == 1;
 		},
 		scenes: {
 			'start': {
 				text: [
-					'the villagers haul a filthy man out of the store room.',
-					"say his folk have been skimming the supplies.",
-					'say he should be strung up as an example.'
+					'村民们从仓库里拽出来一个脏兮兮的家伙。',
+					"据说他的同伙们早已经顺走许多货物。",
+					'他们说应该绞死他以儆效尤。'
 				],
-				notification: 'a thief is caught',
+				notification: '抓到一名盗贼',
 				buttons: {
 					'kill': {
-						text: 'hang him',
+						text: '绞死他',
 						nextScene: {1: 'hang'}
 					},
 					'spare': {
-						text: 'spare him',
+						text: '释放他',
 						nextScene: {1: 'spare'}
 					}
 				}
 			},
 			'hang': {
 				text: [
-			       'the villagers hang the thief high in front of the store room.',
-			       'the point is made. in the next few days, the missing supplies are returned.'
+			       '村民们将盗贼绞死，高挂在仓库的门前。',
+			       '此举卓有成效。数日后，遗失的物资都被还了回来。'
 		        ],
 		        onLoad: function() {
 		        	$SM.set('game.thieves', 2);
@@ -38,15 +38,15 @@ Events.Global = [
 		        },
 		        buttons: {
 					'leave': {
-						text: 'leave',
+						text: '离开',
 						nextScene: 'end'
 					}
 				}
 			},
 			'spare': {
 				text: [
-			       "the man says he's grateful. says he won't come around any more.",
-			       "shares what he knows about sneaking before he goes."
+			       "这家伙感激涕零，说他再也不会来行窃了。",
+			       "在离开前他分享了有关潜行的心得。"
 		        ],
 		        onLoad: function() {
 		        	$SM.set('game.thieves', 2);
@@ -55,7 +55,7 @@ Events.Global = [
 		        },
 		        buttons: {
 		        	'leave': {
-		        		text: 'leave',
+		        		text: '离开',
 		        		nextScene: 'end'
 		        	}
 		        }
